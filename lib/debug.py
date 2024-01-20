dc634dc49b472ca25ff0b308e932f3f687607342
tyler kim <yoonsoo.kim11@gmail.com>
2024-01-20 17:07:02 -0500

    created owner class and mapped Owner class to DB table

--- lib/debug.py
+++ lib/debug.py
@@ -2,7 +2,26 @@
 # lib/debug.py
 
 from models.__init__ import CONN, CURSOR
+from models.owner import Owner
+
+Owner.drop_table()
+Owner.create_table()
+
+
+owner1 = Owner.create("John")
+print(owner1)
+owner2 = Owner.create("Anne")
+print(owner2)
+
+owner1.name = 'John'
+owner1.update()
+
+owner2.delete()
+
+
+
+
+
+
 import ipdb
-
-
 ipdb.set_trace()
--- lib/models/owner.py
+++ lib/models/owner.py
@@ -1,6 +1,75 @@
-from __init__ import CURSOR, CONN
-
+# from __init__ import CURSOR, CONN
+from models.__init__ import CURSOR, CONN
 
 class Owner:
 
-    def __init__(self, name):
+    def __init__(self, name, id=None):
+        self.name = name
+        self.id = id
+
+    def __repr__(self):
+        return f"<Owner {self.id}: {self.name}>"
+
+    @classmethod
+    def create_table(cls):
+        """ Create a new table to persist the attributes of owner instances """
+        sql = """
+            CREATE TABLE IF NOT EXISTS owners (
+            id INTEGER PRIMARY KEY,
+            name TEXT)
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        """ Drop the table that persists Owner instances """
+        sql = """
+            DROP TABLE IF EXISTS owners;
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+
+    def save(self):
+        """ Insert a new row with the name values of the current Owner instance.
+        Update object id attribute using the primary key value of new row.
+        """
+        sql = """
+            INSERT INTO owners (name)
+            VALUES (?)
+        """
+
+        CURSOR.execute(sql, (self.name,))
+        CONN.commit()
+
+        self.id = CURSOR.lastrowid
+
+    @classmethod
+    def create(cls, name):
+        """ Initialize a new Owner instance and save the object to the database """
+        owner = cls(name)
+        owner.save()
+        return owner
+    
+    
+    def update(self):
+        """Update the table row corresponding to the current Owner instance."""
+        sql = """
+            UPDATE owners
+            SET name = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.name, self.id))
+        CONN.commit()
+    
+
+    def delete(self):
+        """Delete the table row corresponding to the current Owner instance"""
+        sql = """
+            DELETE FROM owners
+            WHERE id = ?
+        """
+
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
